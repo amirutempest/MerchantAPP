da3ebef231d103f05d9caabed57d1671ab322d1a
Nur Said Amirudin <saidammir80@gmail.com>
2023-01-08 15:31:39 +0700

    4 commit

--- src/screen/KatalogProduk.jsx
+++ src/screen/KatalogProduk.jsx
@@ -1,11 +1,132 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { View, Text, ScrollView, StyleSheet, TouchableOpacity, Image } from 'react-native'
 
-const KatalogProduk = () => {
+const style = StyleSheet.create({
+    cardKategori: {
+        width: 150,
+        height: 40,
+        marginVertical: 15, 
+        marginLeft: 10,
+        borderRadius: 20,
+        borderWidth: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: '#EA4335'
+    },
+    txtKategori: {
+        color: '#EA4335'
+    },
+    wrapperLink: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        maxWidth: 411,
+        padding: 10,
+        paddingBottom: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    linkProduk: {
+        width: 180,
+        height: 180,
+        borderRadius: 10, 
+        marginRight: 10,
+        marginBottom: 10,
+        backgroundColor: '#fff',
+        elevation: 3
+    },
+    imgProduk: {
+        width: 180,
+        height: 120,
+        borderRadius: 10
+    },
+    txtNamaProduk: {
+        marginTop: 10,
+        marginLeft: 10,
+        fontSize: 16,
+        overflow: 'hidden',
+        fontWeight: '700'
+    },
+    txtHarga: {
+        marginLeft: 10,
+        color: '#EA4335',
+        fontWeight: '500'
+    }
+})
+
+const KatalogProduk = ({navigation}) => {
+    let namaProduk = 'Produk xyz';
+    let harga = 50000;
     return (
         <View>
-            <Text>Ini Halaman Katalog Produk</Text>
-        </View>
+        <ScrollView horizontal>
+            <TouchableOpacity style={style.cardKategori}>
+                <Text style={style.txtKategori}>Kategori Produk</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={style.cardKategori}>
+                <Text style={style.txtKategori}>Kategori Produk</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={style.cardKategori}>
+                <Text style={style.txtKategori}>Kategori Produk</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={style.cardKategori}>
+                <Text style={style.txtKategori}>Kategori Produk</Text>
+            </TouchableOpacity>
+        </ScrollView>
+
+        <ScrollView >
+            <View style={style.wrapperLink}>
+            <TouchableOpacity
+                style={{ alignItems: 'center' }}
+                // onPress={() => navigation.navigate('DetailProduk')}
+                >
+                <View style={style.linkProduk}>
+                    <Image
+                    style={style.imgProduk}
+                    source={require('../asset/image/lokasi.png')} />
+                    <Text style={style.txtNamaProduk}>{namaProduk}</Text>
+                    <Text style={style.txtHarga}>Rp {harga}</Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={{ alignItems: 'center' }}
+                // onPress={() => navigation.navigate('DetailProduk')}
+                >
+                <View style={style.linkProduk}>
+                    <Image
+                    style={style.imgProduk}
+                    source={require('../asset/image/lokasi.png')} />
+                    <Text style={style.txtNamaProduk}>{namaProduk}</Text>
+                    <Text style={style.txtHarga}>Rp {harga}</Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={{ alignItems: 'center' }}
+                // onPress={() => navigation.navigate('DetailProduk')}
+                >
+                <View style={style.linkProduk}>
+                    <Image
+                    style={style.imgProduk}
+                    source={require('../asset/image/lokasi.png')} />
+                    <Text style={style.txtNamaProduk}>{namaProduk}</Text>
+                    <Text style={style.txtHarga}>Rp {harga}</Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={{ alignItems: 'center' }}
+                // onPress={() => navigation.navigate('DetailProduk')}
+                >
+                <View style={style.linkProduk}>
+                    <Image
+                    style={style.imgProduk}
+                    source={require('../asset/image/lokasi.png')} />
+                    <Text style={style.txtNamaProduk}>{namaProduk}</Text>
+                    <Text style={style.txtHarga}>Rp {harga}</Text>
+                </View>
+            </TouchableOpacity>
+            </View>
+            <View style={{marginTop: 20, width: 400, height: 20}}></View>
+        </ScrollView>
+    </View>
     )
 }
 
--- src/screen/KelolaProduk.jsx
+++ src/screen/KelolaProduk.jsx
@@ -1,20 +1,117 @@
 import React, { useState } from 'react'
-import { ScrollView, Text, TextInput, View, StyleSheet } from 'react-native'
-import { Dropdown } from 'react-native-element-dropdown';
+import { ScrollView, Text, TextInput, View, StyleSheet, TouchableOpacity, Image } from 'react-native'
+// import DropDownPicker from 'react-native-dropdown-picker';
 
+const CardProduk = () => {
+    let priceProduk = 50000;
+    let stokJumlah = 99;
+    return (
+        <View style={styles.wrapCard}>
+            <Image
+                style={styles.imgKategori}
+                source={require('../asset/image/lokasi.png')} />
+            <View style={styles.flexCard}>
+                <Text style={styles.txtKategoriName}>Nama Kategori</Text>
+                <Text style={styles.kategori}>Kategori</Text>
+                <Text style={styles.hargaProduk}>Rp {priceProduk}</Text>
+
+                <View style={styles.flexStok}>
+                    <Text style={styles.txtJ}>Stok :</Text>
+                    <Text style={styles.txtR}>{stokJumlah}</Text>
+                </View>
+                <View style={styles.wrapCrud}>
+                    <TouchableOpacity
+                        style={styles.wrapEdit}>
+                        <Image source={require('../asset/icon/edit-ico.png')} />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.wrapDelete}>
+                        <Image source={require('../asset/icon/delete-ico.png')} />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    )
+}
 
 const KelolaProduk = () => {
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(null);
+    const [items, setItems] = useState([
+        { label: 'Apple', value: 'apple' },
+        { label: 'Banana', value: 'banana' }
+    ]);
 
+    let resultJumlah = 9
     return (
         <ScrollView>
             <View>
-                <Text>Form Pemabahan Produk</Text>
-                <View>
+                <Text style={styles.txtForm}>Form Pemabahan Produk</Text>
+                <View style={styles.wrapForm}>
                     <TextInput
+                        style={styles.formKategori}
                         placeholder='Masukan Nama Produk' />
+                    {/* <DropDownPicker
+                        open={open}
+                        value={value}
+                        items={items}
+                        setOpen={setOpen}
+                        setValue={setValue}
+                        setItems={setItems}
+                    /> */}
                     <TextInput
-                    placeholder='Dropdown'/>
+                        style={styles.formKategori}
+                        placeholder='Pilih Kategori' />
+                    <View style={styles.wrapUnggah}>
+                        <Text style={styles.txtUnggah}>Tambahkan Gambar / Icon Produk</Text>
+                        <TouchableOpacity style={styles.touchUnggah}>
+                            <Image source={require('../asset/icon/upload-ico.png')} />
+                            <Text style={styles.txtTouch}>Unggah Gambar</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <TextInput
+                        style={styles.formKategori}
+                        placeholder='Masukan Seri Produk / Nomor Seri'
+                    />
+                    <TextInput
+                        style={styles.formKategori}
+                        placeholder='masukan Deskripsi Produk'
+                    />
+                    <View
+                        style={styles.formHarga}>
+                        <TextInput
+
+                            placeholder='Buat Harga Produk'
+                        />
+                        <Image style={styles.icoHarga} source={require('../asset/icon/money.png')} />
+                    </View>
+                    <View style={styles.flexJumlah}>
+                        <Text style={styles.txtJumlah}>Jumlah Stok Produk</Text>
+                        <View style={styles.flexTouch}>
+                            <TouchableOpacity
+                                style={styles.min}>
+                                <Image source={require('../asset/icon/plus.png')} />
+                            </TouchableOpacity>
+                            <Text style={styles.jumlah}>{resultJumlah}</Text>
+                            <TouchableOpacity
+                                style={styles.plus}>
+                                <Image source={require('../asset/icon/minus.png')} />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
                 </View>
+                <View>
+                    <TouchableOpacity style={styles.tambahBtm}>
+                        <Text style={styles.txtTambah}>Tambah</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <View>
+                <Text style={styles.txtDaftarProduk}>Daftar Yang Sudah Ditambahkan</Text>
+                {/* card produk */}
+                <CardProduk />
+                <CardProduk />
+                <CardProduk />
             </View>
         </ScrollView>
     )
@@ -46,4 +143,141 @@ const styles = StyleSheet.create({
         height: 40,
         fontSize: 16,
     },
+    formKategori: {
+        borderBottomWidth: 1,
+        fontSize: 15,
+        borderColor: '#EA4335',
+        marginTop: 10
+    },
+    wrapForm: {
+        marginHorizontal: 10
+    },
+    txtForm: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        color: '#000',
+        marginTop: 15,
+        marginLeft: 10
+    },
+    txtUnggah: {
+        marginVertical: 10,
+        color: 'grey'
+    },
+    touchUnggah: {
+        height: 140,
+        backgroundColor: '#D0D0D0',
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    txtTouch: {
+        color: 'grey',
+        fontWeight: '500'
+    },
+    formHarga: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderBottomWidth: 1,
+        fontSize: 15,
+        borderColor: '#EA4335',
+        marginTop: 10
+    },
+    icoHarga: {
+        top: 10
+    },
+    flexJumlah: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 20
+    },
+    flexTouch: {
+        flexDirection: 'row',
+    },
+    jumlah: {
+        marginHorizontal: 10,
+        fontSize: 15,
+        fontWeight: 'bold'
+    },
+    tambahBtm: {
+        marginTop: 20,
+        width: 370,
+        height: 45,
+        alignItems: 'center',
+        backgroundColor: '#EA4335',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        borderRadius: 10,
+        elevation: 3
+    },
+    txtTambah: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 18
+    },
+    txtDaftarProduk: {
+        margin: 20,
+        fontWeight: 'bold',
+        fontSize: 18,
+        color: '#000'
+    },
+    // Card Produk
+    wrapCard: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        marginVertical: 5,
+        padding: 10,
+        backgroundColor: '#D0D0D0',
+        height: 100,
+        borderRadius: 10
+    },
+    imgKategori: {
+        height: 80,
+        width: 80
+    },
+    flexCard: {
+        marginLeft: 10,
+    },
+    txtKategoriName: {
+        color: '#000',
+        fontSize: 15
+    },
+    wrapCrud: {
+        flexDirection: 'row',
+        top: 50,
+        left: 170,
+        position: 'absolute'
+    },
+    wrapEdit: {
+        backgroundColor: '#FBBC04',
+        height: 30,
+        width: 40,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    wrapDelete: {
+        backgroundColor: '#EA4335',
+        height: 30,
+        width: 40,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10
+    },
+    flexStok: {
+        flexDirection: 'row'
+    },
+    hargaProduk: {
+        color: '#EA4335'
+    },
+    kategori: {
+        color: 'grey'
+    },
+    txtJ: {
+        color: 'grey'
+    },
+    txtR: {
+        fontWeight: 'bold',
+        color: '#000'
+    }
 });
